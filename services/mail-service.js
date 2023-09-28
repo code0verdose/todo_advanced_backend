bb0b63b687de9311ac630e81482aec1f3b794d34
Danil <mydogfromhell666@gmail.com>
2023-09-28 19:08:02 +0300

    add: registration service

--- services/mail-service.js
+++ services/mail-service.js
@@ -1,3 +1,7 @@
-class MailService {}
+class MailService {
+  async sendActivationMail(to, link) {
+
+  }
+}
 
 module.exports = new MailService();
--- services/user-service.js
+++ services/user-service.js
@@ -1,5 +1,30 @@
+const UserModel = require("../models/user-model");
+const bcrypt = require("bcrypt");
+const uuid = require("uuid");
+const mailService = require("./mail-service");
+const tokenService = require("./token-service");
+const UserDto = require("../dtos/user-dto");
 class UserService {
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({ email });
+    if (candidate) {
+      throw new Error(`Пользователь с таким email уже существует`);
+    }
+    const activationLink = uuid.v4();
+    const hashedPassword = await bcrypt.hash(password, 6);
+    const user = await UserModel.create({
+      email,
+      password: hashedPassword,
+      activationLink,
+    });
+    await mailService.sendActivationMail(email, activationLink);
 
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
 }
 
-module.exports = new UserService()
+module.exports = new UserService();
